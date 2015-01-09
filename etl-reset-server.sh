723982b9a469b94b5d9e325223d3b10d267c0737
Artem Vorotnikov <artem@vorotnikov.me>
2015-01-09 12:58:38 +0300

    update

--- etl-reset-server.sh
+++ etl-reset-server.sh
@@ -6,11 +6,14 @@ sudo rm -rf /srv/ftp/legacy/* && sudo cp -t /srv/ftp/legacy /usr/lib/etlegacy/le
 
 # CFG Update
 cd etl-addons
-cd ZellyLuas && git pull && cd ..
-curl -O http://regex.info/code/JSON.lua
-sudo cp -f -t /etc/xdg/etlegacy/etmain etl_server.cfg legacy.cfg
+
+cd ZellyLuas && git pull && cd .. && curl -O http://regex.info/code/JSON.lua
+
+sudo cp -f -t /etc/xdg/etlegacy/etmain etl_server.cfg legacy.cfg mapvotecycle.cfg
 sudo cp -f -t /usr/lib/etlegacy/legacy/omni-bot/et/user omni-bot.cfg
 sudo cp -f -t /usr/lib/etlegacy/legacy ZellyLuas/ZXpSave.lua JSON.lua
+sudo cp -t /srv/ftp/legacy pk3/* && sudo cp -t /usr/lib/etlegacy/etmain pk3/*
+
 cd ..
 
 # Server restart
--- etl-update-server.sh
+++ etl-update-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 sudo echo "sudo granted..."
-cd etlegacy-git/src/etlegacy && git pull && cd ../../..
-cd etlegacy-mod-win32-git/src/etlegacy && git pull && cd ../../..
+
 curl -O https://aur.archlinux.org/packages/et/etlegacy-git/etlegacy-git.tar.gz && tar -xvf etlegacy-git.tar.gz
 curl -O https://aur.archlinux.org/packages/et/etlegacy-mod-win32-git/etlegacy-mod-win32-git.tar.gz && tar -xvf etlegacy-mod-win32-git.tar.gz
 cd etlegacy-git && makepkg -si --noconfirm && cd ..
